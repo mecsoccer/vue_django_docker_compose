76041cbc6117faf9e9a53f346a33d01f8697205a
Jaachimma Onyenze <ctw02309@L-V4HL0MMWV9.Home>
2023-03-07 15:36:03 +0000

    update post presentation

--- backend/ChargeSessionCost/views.py
+++ backend/ChargeSessionCost/views.py
@@ -1,13 +1,9 @@
-from django.shortcuts import render
 from .models import ChargeSessionCost
 from rest_framework import generics
 from .serializer import ChargeSessionCostSerializer
 from rest_framework.permissions import IsAuthenticated
 
-
 class ChargeSessionCostList(generics.ListAPIView):
-    # API endpoint that allows customer to be viewed.
-    # queryset = ChargeSessionCost.objects.filter()[:15]
     serializer_class = ChargeSessionCostSerializer
     permission_classes = [IsAuthenticated]
     model = ChargeSessionCost
